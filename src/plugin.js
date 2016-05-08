1b39dac192fc97a448271d0250bc6bd58e61022f
Ryan Berdeen <ryan@ryanberdeen.com>
2016-05-08 02:05:19 -0400

    handle multiple chunks, errors

--- src/plugin.js
+++ src/plugin.js
@@ -1,48 +1,46 @@
-const Module = require("module");
-
 module.exports = class BookPlugin {
   apply(compiler) {
     compiler.plugin('emit', (compilation, callback) => {
-      const chunk0 = compilation.chunks[0];
-      if (!chunk0) {
-        // something went wrong. bail without error, something else will show up
-        return callback();
-      }
-
-      const {files} = chunk0;
-      if (files.length !== 1) {
-        return callback(new Error('Expected 1 file output for chunk 0'));
-      }
-
-      const mainSource = compilation.assets[files[0]].source();
-
-      function addAsset({url, html}) {
-        try {
-          html = html(true);
-        } catch (e) {
-          e.file = url;
-          compilation.errors.push(e);
-          html = `build error`;
+      compilation.chunks = compilation.chunks.filter((chunk) => {
+        // skip chunks without a book entry point
+        if (!chunk.entryModule.loaders.find((s) => s.indexOf('book-loader/index.js') >= 0)) {
+          return true;
         }
-        compilation.assets[url] = {
-          source: () => html,
-          size: () => html.length
-        };
-      }
 
-      const main = eval(mainSource);
-      if (!main.require) {
-        return callback(new Error('Entry point is not a page'));
-      }
-      for (let i = 0; i < main.require.m.length; i++) {
-        const mod = main.require(i);
-        if (mod.html && mod.url) {
-          addAsset(mod);
+        const {files} = chunk;
+        if (files.length !== 1) {
+          return true;
         }
-      }
 
-      compilation.chunks.pop();
-      delete compilation.assets[files[0]];
+        function addAsset({url, html}) {
+          try {
+            html = html(true);
+          } catch (e) {
+            e.file = url;
+            compilation.errors.push(e);
+            html = `build error`;
+          }
+          compilation.assets[url] = {
+            source: () => html,
+            size: () => html.length
+          };
+        }
+
+        const mainSource = compilation.assets[files[0]].source();
+        const main = eval(mainSource);
+        main.require.m.forEach((def, i) => {
+          if (def) {
+            const mod = main.require(i);
+            if (mod.html && mod.url) {
+              addAsset(mod);
+            }
+          }
+        });
+
+        delete compilation.assets[files[0]];
+
+        return false;
+      });
 
       callback();
     });
