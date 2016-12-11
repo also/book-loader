4088db40bddd79f70d5ec8a1b9319e9aea57dcab
Ryan Berdeen <ryan@ryanberdeen.com>
2016-12-11 13:11:54 -0500

    more markdown tests

--- src/__tests__/markdown-js-template.js
+++ src/__tests__/markdown-js-template.js
@@ -1,7 +1,7 @@
-function createMd() {
+function createMd(options) {
   const markdownJsTemplate = require('../markdown-js-template');
   const markdownIt = require('markdown-it');
-  return markdownIt().use(markdownJsTemplate);
+  return markdownIt(options).use(markdownJsTemplate);
 }
 
 test('handles links', () => {
@@ -10,14 +10,44 @@ test('handles links', () => {
   expect(result).toMatchSnapshot();
 });
 
-test('allows templates in code blocks', () => {
+test('handles templates in code blocks', () => {
   const md = createMd();
   const result = md.render('```\n<%= require("content") %>\n```');
   expect(result).toMatchSnapshot();
 });
 
-xtest('allows templates in links', () => {
+xtest('handles templates in links', () => {
   const md = createMd();
   const result = md.render(`[link](<%= require('target') %>)`);
+  expect(result).toMatchSnapshot();
+});
+
+test('handles templates at top level', () => {
+  const md = createMd();
+  const result = md.render('# check out this variable\n\n<%= variable %>');
+  expect(result).toMatchSnapshot();
+});
+
+test('handles templates in html block tags', () => {
+  const md = createMd({html: true});
+  const result = md.render('<p><%= variable %></p>');
+  expect(result).toMatchSnapshot();
+});
+
+test('handles templates in html block tags', () => {
+  const md = createMd({html: true});
+  const result = md.render('<p>here is a variable: <%= variable %>!</p>');
+  expect(result).toMatchSnapshot();
+});
+
+test('handles templates in markdown blocks', () => {
+  const md = createMd();
+  const result = md.render('Check out this variable: <%= variable %>');
+  expect(result).toMatchSnapshot();
+});
+
+test('handles templates in inline html', () => {
+  const md = createMd({html: true});
+  const result = md.render('Check out this variable: <code><%= variable %></code>');
   expect(result).toMatchSnapshot();
 });
